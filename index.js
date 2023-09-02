dcc3e62dce3073e51f2364e589dcf3d32ea38264
John Doe <test@email.me>
2023-09-02 13:13:27 +0200

    Add mongoose and establish connection

--- index.js
+++ index.js
@@ -1,58 +1,65 @@
 import express from "express";
 import bodyParser from "body-parser";
+import mongoose from "mongoose";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 const port = 3000;
-
-let todayList = ["srflökgs", "ajdögsd", "skldöjgsd", "dgjs"];
-let workList = [];
+const db = "todoAppDb";
+const _URI = `mongodb://127.0.0.1:27017/${db}`;
 
 app.use(express.static(`${__dirname}/public`));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  res.render(`${__dirname}/views/index.ejs`);
+	res.render(`${__dirname}/views/index.ejs`);
 });
 
 app.post("/", (req, res) => {
-  res.render(`${__dirname}/views/index.ejs`);
+	res.render(`${__dirname}/views/index.ejs`);
 });
 
 app.get("/today", (req, res) => {
-  res.render(`${__dirname}/views/today.ejs`, { todayList: todayList });
+	res.render(`${__dirname}/views/today.ejs`, { todayList: todayList });
 });
 
-app.post("/today", (req, res) => {  
-  if (req.body.textTodoEntry) {
-    todayList[todayList.indexOf("")] = req.body.textTodoEntry
-  }
+app.post("/today", (req, res) => {
+	if (req.body.textTodoEntry) {
+		todayList[todayList.indexOf("")] = req.body.textTodoEntry;
+	}
 
-  if (req.body["newEntry.x"]) {
-    todayList.push("");
-  }
+	if (req.body["newEntry.x"]) {
+		todayList.push("");
+	}
 
-  res.render(`${__dirname}/views/today.ejs`, { todayList: todayList });
+	res.render(`${__dirname}/views/today.ejs`, { todayList: todayList });
 });
 
 app.get("/work", (req, res) => {
-  res.render(`${__dirname}/views/work.ejs`, { workList: workList });
+	res.render(`${__dirname}/views/work.ejs`, { workList: workList });
 });
 
 app.post("/work", (req, res) => {
-  if (req.body.textTodoEntry) {
-    workList[workList.indexOf("")] = req.body.textTodoEntry
-  }
+	if (req.body.textTodoEntry) {
+		workList[workList.indexOf("")] = req.body.textTodoEntry;
+	}
 
-  if (req.body["newEntry.x"]) {
-    workList.push("");
-  }
+	if (req.body["newEntry.x"]) {
+		workList.push("");
+	}
 
-  res.render(`${__dirname}/views/work.ejs`, { workList: workList });
+	res.render(`${__dirname}/views/work.ejs`, { workList: workList });
 });
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
+try {
+	await mongoose.connect(_URI);
+	console.log("Connection established.");
+
+	app.listen(port, () => {
+		console.log(`Server running on port ${port}`);
+	});
+} catch (error) {
+	console.error("Error encountered:", error);
+}
